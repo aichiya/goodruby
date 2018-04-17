8d69dc0908e34d48f38c84c67f90544454bc9f02
Doesnty <doesntalt@gmail.com>
2018-04-17 15:55:25 -0500

    Oops

---
+++ data/battle_anim_new.s
@@ -0,0 +1,35 @@
+	.include "include/macros.inc"
+	.include "constants/constants.inc"
+
+	.section .rodata
+
+	.align 2
+gBattleAnimSpriteTemplate_EnergyBall1::
+	spr_template 10176, 10289, gOamData_837DF94, gDummySpriteAnimTable, NULL, gSpriteAffineAnimTable_83DAEA4, sub_80DDF40
+
+	.align 2
+gBattleAnimSpriteTemplate_EnergyBall2::
+	spr_template 10150, 10289, gOamData_837DFEC, gSpriteAnimTable_83DA274, NULL, gSpriteAffineAnimTable_83DA2B0, sub_80CC6CC
+
+	.align 2
+gBattleAnimSpriteTemplate_LeafStorm::
+	spr_template 10160, 10160, gOamData_837DF54, gSpriteAnimTable_83DA3E0, NULL, gDummySpriteAffineAnimTable, sub_80DA208
+
+	.align 2
+gBattleAnimSpriteTemplate_LeafStorm2:: 
+	spr_template 10063, 10063, gOamData_837DFEC, gSpriteAnimTable_83D6494, NULL, gSpriteAffineAnimTable_83DA2B0, sub_80CC6CC
+
+	.align 2
+gBattleAnimSpriteTemplate_BraveBird::
+	spr_template 10284, 10290, gOamData_837DFFC, gDummySpriteAnimTable, NULL, gDummySpriteAffineAnimTable, sub_80DB5E4
+
+	.align 2
+gBattleAnimSpriteTemplate_FlameBurst1::
+	spr_template 10029, 10029, gOamData_837DF34, gSpriteAnimTable_83D9264, NULL, gDummySpriteAffineAnimTable, sub_80D3554
+
+	.align 2
+gBattleAnimSpriteTemplate_FlameCharge::
+	spr_template 10029, 10029, gOamData_837DF34, gSpriteAnimTable_83D9264, NULL, gDummySpriteAffineAnimTable, sub_80CC6CC
+
+
+
